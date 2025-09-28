20cc684fbdc1a4dab6ae92fadbae2a4ed505e755
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-28 20:57:40 +1000

    My solution

--- 1-working-with-generics/challenge/solution.ts
+++ 1-working-with-generics/challenge/solution.ts
@@ -1,12 +1,20 @@
-// Create a type-safe, generic sorting function for an array of objects 
+// Create a type-safe, generic sorting function for an array of objects
 // based on a property key of the object provided at runtime.
- 
+
 // The function must maintain type safety for property keys.
 
 // Complete this function
-function sortArray() {}
+function sortArray<T extends {}, U extends keyof T>(
+  list: T[],
+  sortKey: U
+): T[] {
+  return list.sort((a: T, b: T) => (a[sortKey] < b[sortKey] ? -1 : 1));
+}
 
 // Test your sorting function with the following array
-let tasks = [{ id: 2, title: "Write Code" }, { id: 1, title: "Compile" }];
-// let sortedTasks = sortArray(tasks, "id");
-// console.log(sortedTasks);  
+let tasks = [
+  { id: 2, title: "Write Code" },
+  { id: 1, title: "Compile" },
+];
+let sortedTasks = sortArray(tasks, "id");
+console.log(sortedTasks);
