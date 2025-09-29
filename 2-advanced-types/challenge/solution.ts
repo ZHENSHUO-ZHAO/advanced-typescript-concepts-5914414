e84c3b7951d133de9b59a24ffb11a6ce0cdb49e7
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-29 13:33:39 +1000

    My solution

--- 2-advanced-types/challenge/solution.ts
+++ 2-advanced-types/challenge/solution.ts
@@ -1,45 +1,59 @@
+type UserType = "Admin" | "Guest" | "Member";
+
+type UserBase = {
+  id: number;
+  username: string;
+};
+
+type User<T extends UserType> = UserBase &
+  (T extends "Admin"
+    ? { adminPermissions: string[] }
+    : T extends "Guest"
+    ? { expirationDate: Date }
+    : { memberSince: Date });
+
 // Replace the three interfaces with a generic interface that can be used to define the three types of users.
-interface Admin {
-  id: number;
-  username: string;
-  adminPermissions: string[];
-}
+// interface Admin {
+//   id: number;
+//   username: string;
+//   adminPermissions: string[];
+// }
 
-interface Guest {
-  id: number;
-  username: string;
-  expirationDate: Date;
-}
+// interface Guest {
+//   id: number;
+//   username: string;
+//   expirationDate: Date;
+// }
 
-interface Member {
-  id: number;
-  username: string;
-  memberSince: Date;
-}
+// interface Member {
+//   id: number;
+//   username: string;
+//   memberSince: Date;
+// }
 
 // Update the function to accept the new generic user type.
-function updateUser(user: Admin | Guest | Member) {
+function updateUser<T extends UserType>(user: User<T>) {
   console.log(`Updating user ${user.username}`);
   // Simulate updating user in the database
 }
 
 // Update the function calls to use the new generic user type.
-const admin: Admin = {
+const admin: User<"Admin"> = {
   id: 1,
   username: "adminUser",
-  adminPermissions: ["manage_system", "modify_users"]
+  adminPermissions: ["manage_system", "modify_users"],
 };
 
-const guest: Guest = {
+const guest: User<"Guest"> = {
   id: 2,
   username: "guestUser",
-  expirationDate: new Date()
+  expirationDate: new Date(),
 };
 
-const member: Member = {
+const member: User<"Member"> = {
   id: 3,
   username: "memberUser",
-  memberSince: new Date()
+  memberSince: new Date(),
 };
 
 updateUser(admin);
