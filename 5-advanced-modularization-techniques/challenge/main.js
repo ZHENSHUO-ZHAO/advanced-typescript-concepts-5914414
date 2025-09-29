43a7b7ee9f94e54a989b13ef78ba2b71934f10b1
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-29 21:33:46 +1000

    My solution

---
+++ 5-advanced-modularization-techniques/challenge/main.js
@@ -0,0 +1,23 @@
+"use strict";
+// import { Analytics } from './analytics';
+// import { UserManagement } from './userManagement';
+const recordBtn = document.getElementById("record-btn");
+if (!recordBtn) {
+    console.error("Element with ID 'record-btn' not found.");
+}
+else {
+    recordBtn.addEventListener("click", async () => {
+        const { Analytics } = await import("./analytics.js");
+        Analytics.recordEvent("Button Clicked");
+    });
+}
+const loadProfileBtn = document.getElementById("load-profile-btn");
+if (!loadProfileBtn) {
+    console.error("Element with ID 'load-profile-btn' not found.");
+}
+else {
+    loadProfileBtn.addEventListener("click", async () => {
+        const { UserManagement } = await import("./userManagement.js");
+        UserManagement.loadUserProfile("user123");
+    });
+}
--- 5-advanced-modularization-techniques/challenge/main.ts
+++ 5-advanced-modularization-techniques/challenge/main.ts
@@ -1,20 +1,22 @@
-import { Analytics } from './analytics';
-import { UserManagement } from './userManagement';
+// import { Analytics } from './analytics';
+// import { UserManagement } from './userManagement';
 
-const recordBtn = document.getElementById('record-btn');
+const recordBtn = document.getElementById("record-btn");
 if (!recordBtn) {
   console.error("Element with ID 'record-btn' not found.");
 } else {
-  recordBtn.addEventListener('click', () => {
-    Analytics.recordEvent('Button Clicked');
+  recordBtn.addEventListener("click", async () => {
+    const { Analytics } = await import("./analytics.js");
+    Analytics.recordEvent("Button Clicked");
   });
 }
 
-const loadProfileBtn = document.getElementById('load-profile-btn');
+const loadProfileBtn = document.getElementById("load-profile-btn");
 if (!loadProfileBtn) {
   console.error("Element with ID 'load-profile-btn' not found.");
 } else {
-  loadProfileBtn.addEventListener('click', () => {
-    UserManagement.loadUserProfile('user123');
+  loadProfileBtn.addEventListener("click", async () => {
+    const { UserManagement } = await import("./userManagement.js");
+    UserManagement.loadUserProfile("user123");
   });
 }
