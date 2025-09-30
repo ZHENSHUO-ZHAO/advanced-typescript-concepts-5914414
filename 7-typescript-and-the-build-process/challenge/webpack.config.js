3500553293feb4a9f6dee1b51d599a24ecb1b9bf
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-30 15:16:26 +1000

    switch to commonjs

--- 7-typescript-and-the-build-process/challenge/webpack.config.js
+++ 7-typescript-and-the-build-process/challenge/webpack.config.js
@@ -1,13 +1,17 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   mode: "production",
   entry: {
     app: "./src/app.ts",
     largeModule: "./src/largeModule.ts",
   },
   output: {
-    filename: "[name]-[contenthash].js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
